bcbefcc9fd98d6d2deec44b80252528c0820115d
Vincent Lainé <vincent.laine.utc@gmail.com>
2025-09-04 13:19:51 +0200

    Initialize NestJS backend and Vue 3 frontend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data.db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
